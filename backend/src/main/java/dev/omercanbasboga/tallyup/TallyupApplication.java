6b23941361eee51138bc075d6e67b80a7e435c71
Omercanbasboga <94013047+Omercanbasboga@users.noreply.github.com>
2026-09-14 11:12:57 +0300

    Add files via upload

---
+++ backend/src/main/java/dev/omercanbasboga/tallyup/TallyupApplication.java
@@ -0,0 +1,11 @@
+package dev.omercanbasboga.tallyup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallyupApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TallyupApplication.class, args);
+    }
+}
